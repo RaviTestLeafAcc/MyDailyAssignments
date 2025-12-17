d009e21ed0fd580d6cea1e8a8dd9d5076f1a9b61
RaviTestLeafAcc <RAVISARAN87@GMAIL.COM>
2025-12-17 18:57:02 +0530

    Add files via upload
    Day1 training assignment files

---
+++ Day1/DataType.js
@@ -0,0 +1,13 @@
+var version = 1.10 //number could be number or decimal
+var empName = "Siva" //string name
+var isAutomated = true //boolean declaration (true or false)
+var value
+//undefined declaration is used when the value is needed as an input in runtime
+
+var data = null // if the user wanted to use null as a value intentionally
+console.log("Software version = " +version, typeof +version)
+console.log("Software version Data Type is " +typeof version)
+
+console.log("Employee Name Data Type: " +typeof(empName))
+console.log("DataTypecheck : " +data +""+typeof(data))
+console.log("Automated dataType: " + typeof(isAutomated))
---
+++ Day1/VariablesLrn.js
@@ -0,0 +1,21 @@
+var input = "10"
+// Reassignment or update the value below is allowed
+var input = "20" 
+console.log(input + " is the var Variable value")
+// Redeclaration of the data type below is allowed
+
+// reassignment is allowed and redeclaration is not allowed
+
+let year = 2025 
+let newdata = 2026
+console.log(newdata + " is the let Variable value")
+
+
+// const = Reassignment and redeclaration is not allowed
+
+const pi = 3.14
+//pi = 3.15 // not allowed
+console.log(pi + " is the const Variable value")
+
+
+
